b99429717d28740a630bac95f011e3207bdeeabd
Liam Sabourin <87916806+lsabs49@users.noreply.github.com>
2024-10-29 20:23:17 -0700

    printing preliminary waveforms

--- ltspiceAnalysis.m
+++ ltspiceAnalysis.m
@@ -21,16 +21,31 @@ bpFilter = designfilt("bandpassiir", ...
     FilterOrder=2,HalfPowerFrequency1=0.5, ...
     HalfPowerFrequency2=100,SampleRate=sampleRate);
 
-freqz(bpfilt,[],sampleRate);
+freqz(bpFilter,[],sampleRate); %Display frequency spectrum of filter
 
-bandpassECG = filtfilt(bpFilter,noisyECG);
+%generating parameters for Notch
+Nyquist = sampleRate/2;
+Q = 2;
+f1 = 50 - Q/2;
+f2 = 50 + Q/2;
+
+bsFilter = designfilt('bandstopiir', ...
+    'FilterOrder',2,'StopbandFrequency1', ...
+    f1,'StopbandFrequency2',f2, ...
+    'StopbandAttenuation',60, ...
+    'SampleRate',44100);
+
+freqz(bsFilter,[],sampleRate); %Display frequency spectrum of filter
+
+bpECG = filtfilt(bpFilter,noisyECG);
+bpbsECG = filtfilt(bsFilter,bpECG);
 
 % Calculate time vector
 timeVector = 0:seconds(1/sampleRate):seconds(sampleRange(2)/sampleRate);
 timeVector = timeVector(1:end-1);
 
 %plotting
-plot(t, noisyECG, t, analogFilteredECG, t, bandpassECG, t, cleanECG);
+plot(t, noisyECG, t, analogFilteredECG, t, bpbsECG, t, cleanECG);
 xlabel('Time');
 ylabel('Audio Signal');
 
